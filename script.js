1bcb76ee898fc6422c719e91783677ccc76fc5c8
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-09-11 13:20:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function firstWord(s) {
   // your code here
 	const regex = /^\w+/;
-	const check = s.match(regex);
+	const check = s.trim().match(regex);
 	return check ? check[0] : "";
 }
 const s = prompt("Enter String:");
