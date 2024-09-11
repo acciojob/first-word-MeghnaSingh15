23eef96a206117d5edfc80fd95e689bb045add80
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-09-11 13:08:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 function firstWord(s) {
   // your code here
 	const regex = /^\w+/;
-	const check = s.match( /^\w+/;);
-	console.log(check[0]);
-
+	const check = s.match(regex);
+	return check ? check[0] : "";
 }
-
-// Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
